2d5e9008c7be25a45fb353ee0efb21912ca0a356
Magnus Ramsfjell <mramsfjell@gmail.com>
2020-01-16 13:19:16 +0100

    Updated go code to be less c-like.

--- ex1/part4go.go
+++ ex1/part4go.go
@@ -8,17 +8,15 @@ import (
 
 var i = 0
 
-func incrementing(i *int) {
-	//TODO: increment i 10000000 times
+func incrementing() {
 	for j := 0; j < 10000000; j++ {
-		(*i)++
+		i++
 	}
 }
 
-func decrementing(i *int) {
-	//TODO: decrement i 1000000 times
+func decrementing() {
 	for j := 0; j < 10000000; j++ {
-		(*i)--
+		i--
 	}
 }
 
@@ -26,13 +24,11 @@ func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU()) // Manually set high to ensure data races
 	// Try doing the exercise both with and without it!
 
-	// TODO: Maybe do this in the go way, not the c way with pointers
-	go incrementing(&i)
-	go decrementing(&i)
+	go incrementing()
+	go decrementing()
 
 	// We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
 	// We'll come back to using channels in Exercise 2. For now: Sleep.
 	time.Sleep(100 * time.Millisecond)
 	Println("The magic number is:", i)
 }
-
