a22583a56343368911e826dafcafbf61ba00864d
Magnus Ramsfjell <mramsfjell@gmail.com>
2020-01-18 15:39:30 +0100

    Added protection to go-code.

--- ex2/Part2/go/foo.go
+++ ex2/Part2/go/foo.go
@@ -3,8 +3,8 @@
 package main
 
 import (
-    . "fmt"
-    "runtime"
+	. "fmt"
+	"runtime"
 )
 
 // Control signals
@@ -19,37 +19,60 @@ func number_server(add_number <-chan int, control <-chan int, number chan<- int)
 	// This for-select pattern is one you will become familiar with if you're using go "correctly".
 	for {
 		select {
-			// TODO: receive different messages and handle them correctly
-			// You will at least need to update the number and handle control signals.
+		// Receive different messages and handle them correctly
+		// You will at least need to update the number and handle control signals.
+			case j := <-add_number:
+				i += j
+			case status := <-control:
+				if status == GetNumber {
+					Println("Received 1st on control line")
+					number <- i
+				} else {
+					Println("Received 2nd on control line") // Sometimes doesn't run (main might exit before it gets to?)
+					return
+				}
 		}
 	}
 }
 
-func incrementing(add_number chan<-int, finished chan<- bool) {
-	for j := 0; j<1000000; j++ {
+func incrementing(add_number chan<- int, finished chan<- bool) {
+	for j := 0; j < 1000000; j++ {
 		add_number <- 1
 	}
-	//TODO: signal that the goroutine is finished
+	// Signal that the goroutine is finished
+	Println("Finished incrementing")
+	finished <- true
 }
 
 func decrementing(add_number chan<- int, finished chan<- bool) {
-	for j := 0; j<1000000; j++ {
+	for j := 0; j < 1000000; j++ {
 		add_number <- -1
 	}
-	//TODO: signal that the goroutine is finished
+	// Signal that the goroutine is finished
+	Println("Finished decrementing")
+	finished <- true
 }
 
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
+	runtime.GOMAXPROCS(16) // Manually set high instead of runtime.NumCPU() to ensure proper concurrency
 
-	// TODO: Construct the required channels
-	// Think about wether the receptions of the number should be unbuffered, or buffered with a fixed queue size.
+	// Construct the required channels
+	// Think about whether the receptions of the number should be unbuffered, or buffered with a fixed queue size.
+	control := make(chan int)
+	add_number := make(chan int)
+	number := make(chan int)
+	finished := make(chan bool)
 
-	// TODO: Spawn the required goroutines
+	// Spawn the required goroutines
+	go number_server(add_number, control, number)
+	go incrementing(add_number, finished)
+	go decrementing(add_number, finished)
 
-	// TODO: block on finished from both "worker" goroutines
+	// Block on finished from both "worker" goroutines
+	_, _ = <-finished, <-finished
 
-	control<-GetNumber
-	Println("The magic number is:", <- number)
-	control<-Exit
+	control <- GetNumber
+	Println("The magic number is:", <-number)
+	control <- Exit
 }
+
