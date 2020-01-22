63df2bdd16cdb3e4ed30dba40cd0a4e794c81809
Magnus Ramsfjell <mramsfjell@gmail.com>
2020-01-22 21:35:13 +0100

    Added protection to python code

--- ex2/Part2/python/foo.py
+++ ex2/Part2/python/foo.py
@@ -1,34 +1,48 @@
 # Python 3.3.3 and 2.7.6
-# python fo.py
+# python part4py.py
 
 from threading import Thread
+from threading import Lock
 
 # Potentially useful thing:
 #   In Python you "import" a global variable, instead of "export"ing it when you declare it
 #   (This is probably an effort to make you feel bad about typing the word "global")
 i = 0
+lock = Lock()
 
 def incrementingFunction():
     global i
-    # TODO: increment i 1_000_000 times
+
+    for j in range(1000000):
+      lock.acquire()
+      try:
+        i = i+1
+      finally:
+        lock.release()
 
 def decrementingFunction():
     global i
-    # TODO: decrement i 1_000_000 times
+
+    for j in range(1000000):
+      lock.acquire()
+      try:
+        i = i-1
+      finally:
+        lock.release()
 
 
 
 def main():
-    global i
 
     incrementing = Thread(target = incrementingFunction, args = (),)
     decrementing = Thread(target = decrementingFunction, args = (),)
-    
-    # TODO: Start both threads
-    
+
+    incrementing.start()
+    decrementing.start()
+
     incrementing.join()
     decrementing.join()
-    
+
     print("The magic number is %d" % (i))
 
 
